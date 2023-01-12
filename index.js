13e194fc992feeb1d65286907b1b192ebae14e35
Siddharth verma <990.siddharthverma@gmail.com>
2023-01-13 02:10:06 +0530

    Updated
    Final Commit

--- index.js
+++ index.js
@@ -5,6 +5,30 @@ const checkLucky = document.querySelector("#check");
 checkLucky.addEventListener("click", checkBirthday);
 
 
+function validatedValue(sum, luckyNumber){
+    if(sum%luckyNumber===0){
+        text.innerText =  "Your Birthday is Lucky ✨"
+    } else {
+         text.innerText = "Not a Lucky birthdate 🙂"
+    }
+}
+
 function checkBirthday() {
-    console.log(birthDate.value, luckyNumber.value)
+    const  birthday =  birthDate.value;
+    const sum = checkSum(birthday);
+    if(sum&&birthday) 
+    validatedValue(sum, luckyNumber.value);
+    else
+    text.innerText = "Please Enter the Both Details"
+}
+
+function checkSum(birthday){
+     
+    birthday = birthday.replaceAll("-", "");
+    let sum = 0;
+  for(let i=0;i< birthday.length;i++){
+    sum += Number(birthday.charAt(i));
+  }
+ return sum;  
+    
 }
